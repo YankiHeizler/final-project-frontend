dd168e9c41503c0410eb94fcb12eb4d2528e47ac
MosheB <mosesbirnbaum@gmail.com>
2024-05-12 12:34:50 +0300

    Basic Header

--- src/Components/Header/HeaderComp.jsx
+++ src/Components/Header/HeaderComp.jsx
@@ -1,11 +1,19 @@
-import logo_image from '../../assets/Images/logo/logo_image.png'
+import { Link } from 'react-router-dom'
+import logo_image from '../../assets/Images/logo/logo_image.png' // Assuming logo image
 import './Header.css'
+
 const HeaderComp = () => {
   return (
-    <>
-    <img className='logo' src = {logo_image} />
-    </>
+    <header className='header-container'>
+      <nav>
+        <button><Link to={'/'} className='link'>Home Page</Link></button>
+        <button><Link to={'/login'} className='link'>Login</Link></button>
+      </nav>
+      <p className='heading'>Best place to study</p>
+      <img className='logo' src={logo_image} alt='Company Logo' />
+    </header>
   )
 }
 
 export default HeaderComp
+
--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import HeaderComp from '../../Components/Header/HeaderComp'
 
+
 const HomePage = () => {
   return (
 
     <>
     <HeaderComp/>
+
     </>
   )
 }
