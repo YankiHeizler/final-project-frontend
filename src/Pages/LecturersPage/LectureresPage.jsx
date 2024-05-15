b5d479e7f9f76a99d9600787cd3e0fbc9f301372
MosheB <mosesbirnbaum@gmail.com>
2024-05-16 00:37:15 +0300

    Moshe1

--- src/Pages/LecturersPage/LectureresPage.jsx
+++ src/Pages/LecturersPage/LectureresPage.jsx
@@ -1,6 +1,8 @@
 import { useParams } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import HeaderComp from '../../Components/Header/HeaderComp';
+
 
 const LecturersByLanguage = () => {
   const { language } = useParams(); 
@@ -12,7 +14,7 @@ const LecturersByLanguage = () => {
       try {
         const response = await axios.get(`http://localhost:3008/api/lectors?lecLangs=${language}`);
         const fetchedLecturers = response.data.lectors || [];
-        console.log({language})
+
         console.log(response.data.lectors);
         const filteredLecturers = fetchedLecturers.filter(lecturer =>
           lecturer.lecLangs && lecturer.lecLangs.includes(language)
@@ -21,7 +23,7 @@ const LecturersByLanguage = () => {
         setLecturers(filteredLecturers);
         console.log(lecturers)
         setError(null);
-      } catch (err) {
+      } catch (err) { 
         console.error('Error fetching lecturers:', err);
         setError(err.message);
       }
@@ -34,15 +36,46 @@ const LecturersByLanguage = () => {
     return <div className="error">Error: {error}</div>;
   }
 
-  return (
-    <div>
-      <h1>מרצים שמלמדים {language}</h1>
-      Display lecturers here
-      {lecturers.map((lecturer) => (
-        <p key={lecturer._id}>{lecturer.lecLName}</p>
-      ))}
-    </div>
-  );
+  const LecturerCard = ({ product }) => {
+    return (
+      <div className="product-card" key={product._id}>
+        <div className="product-details">
+          <h3>{product.lecFName}</h3>
+          <h3>{product.lecLName}</h3>
+          {/* <Link to={`/product/${product._id}`}>לפרטים נוספים</Link> */}
+        </div>
+      </div>
+    );
+  };
+  
+  const ProductList = ({ products }) => {
+    return (
+      <div className="product-list">
+        {products.map((product) => (
+          <LecturerCard key={product._id} product={product} />
+        ))}
+      </div>
+    );
+  };
+
+//   return (
+//     <div>
+//       <h1>מרצים שמלמדים </h1>
+
+//       {lecturers.map((lecturer) => (
+//         <p key={lecturer._id}>{lecturer.lecLName}</p>
+//       ))}
+//     </div>
+//   );
+// };
+
+return (
+  <div>
+    <HeaderComp/>
+    <h1>מרצים</h1>
+    <ProductList products={lecturers} />
+  </div>
+);
 };
 
 export default LecturersByLanguage;
