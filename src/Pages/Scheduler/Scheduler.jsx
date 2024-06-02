e72ab9baa2742b95aa547708589e9a18a7e04559
shraga <ardttbdk@gmail.com>
2024-06-02 11:55:48 +0300

    added connection table

--- src/Pages/Scheduler/Scheduler.jsx
+++ src/Pages/Scheduler/Scheduler.jsx
@@ -1,6 +1,9 @@
 import Calendar from "../../Components/Calendar/Calendar.jsx";
+import { getConecshen } from "./Scheduler.helper.jsx";;
+
 // import { /*studentScheduleActive,*/ lecturerSchedule } from "./Scheduler.helper.js";
 import { getStudentData } from "./Scheduler.helper.jsx";
+import { getLecData } from "./Scheduler.helper.jsx";
 import { useEffect, useState } from "react";
 import HeaderComp from '../../Components/Header/HeaderComp.jsx'
 import "./Scheduler.css";
@@ -8,7 +11,24 @@ import "./Scheduler.css";
 // בקשת גט לקבל קולקשןקנקשן שם מלא של מרצה ושם מקצוע 
 // לשלוח בקשת גט עם איידיי של סטודנט שמקבלים מהטוקן הבקשה לconnectionStudLecModel.js
 // הכתובת app.use('/api/connectionStudLec', connectionStudLecRouter
-const lecturers = []
+// function lecturers() {
+//   const [currentConection, setCurrentConection] = useState({});
+
+//   const fetchLecConecshen = async () => {
+//     const dataconecshen = await getConecshen()
+//     setCurrentConection(dataconecshen)
+//   }
+//   useEffect(() => {
+//     fetchLecConecshen()
+//   }, [])
+
+//   const handleConnectionClick = (connection) => {
+//     setCurrentConection(connection);
+//   };
+
+
+// }
+// const lecturers = []
 //   { id: 0, name: 'your schedule', schedule: getStudentData() },
 //   { id: 1, name: "Lecturer 1", schedule: lecturerSchedule },
 //   { id: 2, name: "Lecturer 2", schedule: lecturerSchedule },
@@ -17,23 +37,34 @@ const lecturers = []
 
 function Schedulerr() {
   const [currentSchedule, setCurrentSchedule] = useState({});
+  const [currentConectionSchedule,setCurrentConectionSchedule] =useState({});
+  const [lecturers, setLecturers] = useState([])
 
   const fetchStudentData = async () => {
-   const data = await getStudentData()
-   console.log(data);
-   setCurrentSchedule(data)
-    // setCurrentSchedule()
+    const dataa = await getStudentData()
+    setCurrentSchedule(dataa)
+  }
+  const fetchLecData = async()=>{
+    const Data = await  getLecData()
+    setCurrentConectionSchedule(Data)
+  }
+
+  const fetchConnections = async () => {
+    const data = await getConecshen()
+    console.log(data);
+    setLecturers(data.connectionStudLec)
   }
 
   useEffect(() => {
     fetchStudentData()
+    fetchConnections()
+    fetchLecData()
   }, [])
 
   const handleLecturerClick = (schedule) => {
     setCurrentSchedule(schedule);
   };
   return (
-
     <div className="App">
       <HeaderComp />
       <div className="main-content">
@@ -41,20 +72,21 @@ function Schedulerr() {
         <div className="lecturers-list">
 
           <h2>Lecturers</h2>
+          
           <ul>
-            {lecturers?.map((lecturer) => (
+            {lecturers?.map(lecturer => (
               <li
-                key={lecturer.id}
+                key={lecturer._id}
                 onClick={() => handleLecturerClick(lecturer.schedule)}
               >
-                {lecturer.name}
-               </li>
+                {lecturer.lecID.lecFName}-{lecturer.lecID.lecLName} :{lecturer.connLang}
+              </li>
             ))}
           </ul>
         </div>
       </div>
     </div>
-  );  
- }
+  );
+}
 
 export default Schedulerr;
